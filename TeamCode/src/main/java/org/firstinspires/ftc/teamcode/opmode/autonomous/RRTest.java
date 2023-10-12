47200230cbea558785d06398309b4f3e4e940dd3
CMP-18996 <calculusgeek11@gmail.com>
2023-10-12 17:09:07 -0400

    rr installation

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/RRTest.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.opmode.autonomous;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import org.firstinspires.ftc.teamcode.common.drive.MecanumDrive;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class RRTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+
+        waitForStart();
+
+        Actions.runBlocking(
+                    drive.actionBuilder(drive.pose)
+                            .splineTo(new Vector2d(30, 30), Math.PI / 2)
+                            .splineTo(new Vector2d(60, 0), Math.PI)
+                            .waitSeconds(5)
+                            .build());
+    }
+}
