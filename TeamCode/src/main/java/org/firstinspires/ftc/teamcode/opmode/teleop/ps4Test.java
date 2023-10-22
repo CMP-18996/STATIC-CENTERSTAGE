cc634d53ad1d73dff9c8be5cf5307eeeb1a9d1b7
CMP-18996 <calculusgeek11@gmail.com>
2023-10-21 20:58:18 -0400

    ps4 test asdnfasdjik

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/ps4Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/ps4Test.java
@@ -7,10 +7,11 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.common.Robot;
 import org.firstinspires.ftc.teamcode.common.drive.Drive;
 
-@TeleOp(name = "defaultDriveTest")
+@TeleOp(name = "ps4Test")
 public class ps4Test extends CommandOpMode {
     public Robot robot;
     public Drive drive;
+    public double x1, y1, x2, y2;
     @Override
     public void initialize() {
         telemetry.addData("Status","Initalizing...");
@@ -27,7 +28,21 @@ public class ps4Test extends CommandOpMode {
     @Override
     public void run() {
         CommandScheduler.getInstance().run();
-        drive.manualPower(-gamepad1.left_stick_x, gamepad1.left_stick_y, -gamepad1.right_stick_x);
+        x1 = gamepad1.touchpad_finger_1_x;
+        y1 = gamepad1.touchpad_finger_1_y;
+        x2 = gamepad1.touchpad_finger_2_x;
+        y2 = gamepad1.touchpad_finger_2_y;
+        if (gamepad1.touchpad && x1 > 0.4) {
+            drive.manualPower(0.35, 0, 0);
+        } else if (gamepad1.touchpad && x1 < -0.4) {
+            drive.manualPower(-0.35, 0, 0);
+        } else if (gamepad1.touchpad && y1 > 0.4) {
+            drive.manualPower(0, 0.35, 0);
+        } else if (gamepad1.touchpad && y1 < -0.4) {
+            drive.manualPower(0, -0.35, 0);
+        } else {
+            drive.manualPower(0, 0, 0);
+        }
         telemetry.addData("Status", "Rumning...");
         telemetry.addData("Touchpad X", gamepad1.touchpad_finger_1_x);
         telemetry.addData("Touchpad Y", gamepad1.touchpad_finger_1_y);
