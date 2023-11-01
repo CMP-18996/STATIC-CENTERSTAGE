2b2feb05285272e4531054e6ca8943f678da4d16
donaldzhuang <donaldzhuang08@gmail.com>
2023-10-31 20:19:44 -0400

    color sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/tests/ColorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/tests/ColorTest.java
@@ -31,7 +31,7 @@ public class ColorTest extends LinearOpMode {
                 if(425>= colorSensor.red() && colorSensor.red() >= 345){
                     if(607>= colorSensor.blue() && colorSensor.blue() >= 527){
                         if(685>= colorSensor.green() && colorSensor.green() >= 605){
-                            telemetry.addLine("white");
+                            telemetry.addData("color", "white");
                             telemetry.update();
                         }
                     }
@@ -39,7 +39,7 @@ public class ColorTest extends LinearOpMode {
                 else if(150>= colorSensor.red() && colorSensor.red() >= 70){
                     if(193>= colorSensor.blue() && colorSensor.blue() >= 113){
                         if(299>= colorSensor.green() && colorSensor.green() >= 219){
-                            telemetry.addLine("green");
+                            telemetry.addData("color", "green");
                             telemetry.update();
                         }
                     }
@@ -47,7 +47,7 @@ public class ColorTest extends LinearOpMode {
                 else if(275>= colorSensor.red() && colorSensor.red() >= 195){
                     if(199>= colorSensor.blue() && colorSensor.blue() >= 119){
                         if(383>= colorSensor.green() && colorSensor.green() >= 303){
-                            telemetry.addLine("yellow");
+                            telemetry.addData("color", "yellow");
                             telemetry.update();
                         }
                     }
@@ -55,7 +55,7 @@ public class ColorTest extends LinearOpMode {
                 else if(219>= colorSensor.red() && colorSensor.red() >= 159){
                     if(415>= colorSensor.blue() && colorSensor.blue() >= 335){
                         if(332>= colorSensor.green() && colorSensor.green() >= 252){
-                            telemetry.addLine("purple");
+                            telemetry.addData("color", "purple");
                             telemetry.update();
                         }
                     }
