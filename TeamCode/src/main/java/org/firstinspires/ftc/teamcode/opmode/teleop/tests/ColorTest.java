b1418ed0bf44e85c2cf8e9c5caa1653d2c7a01ff
donaldzhuang <donaldzhuang08@gmail.com>
2023-11-07 19:56:01 -0500

    color sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/tests/ColorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/tests/ColorTest.java
@@ -10,49 +10,81 @@ import com.acmerobotics.dashboard.FtcDashboard;
 @TeleOp
 public class ColorTest extends LinearOpMode {
         // Define a variable for our color sensor
-        ColorSensor colorSensor;
-        int r, b, g;
-        String color;
+        ColorSensor colorSensor1;
+        ColorSensor colorSensor2;
+        int r1, b1, g1, r2, b2, g2;
+        String color1;
+        String color2;
         @Override
         public void runOpMode() {
             // Get the color sensor from hardwareMap
-            colorSensor = hardwareMap.get(ColorSensor.class, "Color");
-            colorSensor.enableLed(false);
+            colorSensor1 = hardwareMap.get(ColorSensor.class, "Color1");
+            colorSensor2 = hardwareMap.get(ColorSensor.class, "Color2");
+            colorSensor1.enableLed(false);
+            colorSensor2.enableLed(false);
             // Wait for the Play button to be pressed
             telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
             waitForStart();
             // While the Op Mode is running, update the telemetry values.
             while (opModeIsActive()) {
-                r = colorSensor.red();
-                g = colorSensor.green();
-                b = colorSensor.blue();
-                telemetry.addData("red", r);
-                telemetry.addData("blue", b);
-                telemetry.addData("green", g);
+                r1 = colorSensor1.red();
+                g1 = colorSensor1.green();
+                b1 = colorSensor1.blue();
 
             }
-            if(g< 200 && b< 200 && r< 200){
-                telemetry.addData("color", "black");
-                color = "black";
+            if(g1< 200 && b1< 200 && r1< 200){
+                telemetry.addData("color1", "black");
+                color1 = "black";
             }
-                else if (g > b && b > r) {
-                    if (g > 400) {
-                        telemetry.addData("color", "white");
-                        color = "white";
+                else if (g1 > b1 && b1 > r1) {
+                    if (g1 > 400) {
+                        telemetry.addData("color1", "white");
+                        color1 = "white";
                     } else {
-                        telemetry.addData("color", "green");
-                        color = "green";
+                        telemetry.addData("color1", "green");
+                        color1 = "green";
                     }
-                } else if (g > r && r > b) {
-                    telemetry.addData("color", "yellow");
-                color = "yellow";
-                } else if (b > g && g > r) {
-                    telemetry.addData("color", "purple");
-                color = "purple";
+                } else if (g1 > r1 && r1 > b1) {
+                    telemetry.addData("color1", "yellow");
+                color1 = "yellow";
+                } else if (b1 > g1 && g1> r1) {
+                    telemetry.addData("color1", "purple");
+                color1 = "purple";
                 }
                 else {
-                telemetry.addData("color", "none");
-                color = "none";
+                telemetry.addData("color1", "none");
+                color1 = "none";
+
+            }
+
+            while (opModeIsActive()) {
+                r2 = colorSensor2.red();
+                g2 = colorSensor2.green();
+                b2 = colorSensor2.blue();
+
+            }
+            if(g2< 200 && b2< 200 && r2< 200){
+                telemetry.addData("color2", "black");
+                color2 = "black";
+            }
+            else if (g2 > b2 && b2 > r2) {
+                if (g2 > 400) {
+                    telemetry.addData("color2", "white");
+                    color2 = "white";
+                } else {
+                    telemetry.addData("color2", "green");
+                    color2 = "green";
+                }
+            } else if (g2 > r2 && r2 > b2) {
+                telemetry.addData("color2", "yellow");
+                color2 = "yellow";
+            } else if (b2 > g2 && g2> r2) {
+                telemetry.addData("color2", "purple");
+                color2 = "purple";
+            }
+            else {
+                telemetry.addData("color2", "none");
+                color2 = "none";
 
             }
                 telemetry.update();
