a10e76dbed4b2ff9ad95412b64100a7d1b41557e
donaldzhuang <donaldzhuang08@gmail.com>
2023-11-11 16:29:01 -0500

    color sensor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystems/LiftSubsystem.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.common.subsystems;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import org.firstinspires.ftc.teamcode.common.Robot;
+import org.firstinspires.ftc.teamcode.common.commandbase.IntakeWait;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class LiftSubsystem extends SubsystemBase{
+
+}
