7b2e8d107af067afcc677e24b0096bdf5767411e
Matthew <matthewzhao105@gmail.com>
2023-11-11 20:38:57 -0500

    probably doesn't work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystems/LiftSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/subsystems/LiftSubsystem.java
@@ -8,9 +8,26 @@ import org.firstinspires.ftc.teamcode.common.commandbase.IntakeWait;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class LiftSubsystem extends SubsystemBase{
     private Motor motorUp, motorDown;
     private int upPower, downPower;
+    private double baseHeight, heightLevelOne, heightLevelTwo, heightLevelThree, heightLevelFour, heightLevelFive;
+    private DistanceSensor heightDetector;
+
+    public enum LiftHeights {
+        // Change the values for the actual robot, otherwise it'll probably crash
+        BASE(0.0),
+        HEIGHTONE(1.0),
+        HEIGHTTWO(2.0),
+        HEIGHTTHREE(3.0),
+        HEIGHTFOUR(4.0),
+        HEIGHTFIVE(55.0);
+
+        LiftHeights(double height) {
+
+        }
+    }
 }
