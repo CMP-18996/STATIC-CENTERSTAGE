0eb4f302ad319e53eb5990cbfb824a15723bb3b6
CMP-18996 <calculusgeek11@gmail.com>
2023-11-26 15:48:06 -0500

    teleop button mapping frameowkr

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/finalized/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/finalized/Teleop.java
@@ -2,14 +2,23 @@ package org.firstinspires.ftc.teamcode.opmode.teleop.finalized;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.common.GlobalVariables;
 import org.firstinspires.ftc.teamcode.common.Robot;
+import org.firstinspires.ftc.teamcode.common.drive.Drive;
+import org.firstinspires.ftc.teamcode.common.subsystems.TouchpadSubsystem;
 
 @TeleOp(name="Final TeleOp", group="Official")
 public class Teleop extends CommandOpMode {
     private Robot robot;
+    private Drive drive;
+    private GamepadEx drivePad;
+    private GamepadEx liftPad;
+    private TouchpadSubsystem touchpad;
     @Override
     public void initialize() {
         telemetry.addData("Status", "Initializing...");
@@ -18,7 +27,14 @@ public class Teleop extends CommandOpMode {
         GlobalVariables.opMode = GlobalVariables.OpMode.TELEOP;
 
         robot = new Robot(hardwareMap);
-        schedule();
+        drive = new Drive(robot);
+        drivePad = new GamepadEx(gamepad1);
+        liftPad = new GamepadEx(gamepad2);
+        touchpad = new TouchpadSubsystem(gamepad2);
+        register(drive, touchpad);
+
+        liftPad.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER)
+                .whenPressed(() -> schedule(new InstantCommand(() -> touchpad.deleteLastInput())));
 
         telemetry.addData("Status", "Initialized!");
         telemetry.update();
@@ -28,7 +44,10 @@ public class Teleop extends CommandOpMode {
     public void run() {
         CommandScheduler.getInstance().run();
 
+        drive.manualPower(-drivePad.getLeftX(), drivePad.getLeftY(), -drivePad.getRightX());
+
         telemetry.addData("Status", "Running...");
+        telemetry.addData("Lift Positions", touchpad.getHistory());
         telemetry.update();
     }
 }
