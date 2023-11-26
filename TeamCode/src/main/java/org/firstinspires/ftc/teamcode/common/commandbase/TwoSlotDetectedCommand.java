6c593bc60575a3df7798627632132ab20bd3a072
donaldzhuang <donaldzhuang08@gmail.com>
2023-11-25 20:28:14 -0500

    color sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/TwoSlotDetectedCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/TwoSlotDetectedCommand.java
@@ -2,9 +2,17 @@ package org.firstinspires.ftc.teamcode.common.commandbase;
 
 import com.arcrobotics.ftclib.command.CommandBase;
 
+import org.firstinspires.ftc.teamcode.common.subsystems.IntakeSubsystem;
+
 public class TwoSlotDetectedCommand extends CommandBase {
+    IntakeSubsystem intakeSubsystem;
+
+    public TwoSlotDetectedCommand(IntakeSubsystem intakeSubsystem) {
+        this.intakeSubsystem = intakeSubsystem;
+    }
+
     @Override
     public boolean isFinished() {
-        return true; // return {function that returns true or false depending on if both slots are filled}
+        return intakeSubsystem.checkFilled(); // return {function that returns true or false depending on if both slots are filled}
     }
 }
