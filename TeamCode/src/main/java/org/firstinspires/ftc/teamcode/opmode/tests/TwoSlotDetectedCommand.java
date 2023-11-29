7f1dcbbceb0a0febb09818a14fbdb4c5f859942c
donaldzhuang <donaldzhuang08@gmail.com>
2023-11-28 20:34:04 -0500

    color sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tests/TwoSlotDetectedCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tests/TwoSlotDetectedCommand.java
@@ -4,6 +4,19 @@ import com.arcrobotics.ftclib.command.CommandBase;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.common.subsystems.IntakeSubsystem;
+import com.arcrobotics.ftclib.util.Timing;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.arcrobotics.ftclib.hardware.motors.Motor.Encoder;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.common.Robot;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
 @TeleOp(name="Slots Test",group="test")
 public class TwoSlotDetectedCommand extends CommandBase {
     IntakeSubsystem intakeSubsystem;
