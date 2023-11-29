611d5bb10d9eaa0f8170abefeb93bc4aa058f1d2
Matthew <matthewzhao105@gmail.com>
2023-11-28 21:29:06 -0500

    Teleop Gamepad

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/Teleop.java
@@ -10,6 +10,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.common.GlobalVariables;
 import org.firstinspires.ftc.teamcode.common.Robot;
 import org.firstinspires.ftc.teamcode.common.drive.Drive;
+import org.firstinspires.ftc.teamcode.common.subsystems.DepositSubsystem;
+import org.firstinspires.ftc.teamcode.common.subsystems.LiftSubsystem;
 import org.firstinspires.ftc.teamcode.common.subsystems.TouchpadSubsystem;
 
 /**
@@ -23,8 +25,19 @@ public class Teleop extends CommandOpMode {
     private GamepadEx drivePad;
     private GamepadEx liftPad;
     private TouchpadSubsystem touchpad;
+    private LiftSubsystem liftSubsystem;
+    private DepositSubsystem depositSubsystem;
     private int row;
     private int column;
+    private ChoiceState currentChoice = ChoiceState.LEFT;
+    public enum ChoiceState {
+        LEFT,
+        RIGHT
+    }
+    public enum SelectionState {
+        CHOOSINGROW,
+        CHOOSINGCOLUMN
+    }
     @Override
     public void initialize() {
         CommandScheduler.getInstance().reset();
@@ -35,13 +48,26 @@ public class Teleop extends CommandOpMode {
         drivePad = new GamepadEx(gamepad1);
         liftPad = new GamepadEx(gamepad2);
         touchpad = new TouchpadSubsystem(gamepad2);
+        liftSubsystem = new LiftSubsystem(robot);
+        depositSubsystem = new DepositSubsystem(robot);
         register(drive, touchpad);
 
         liftPad.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER)
-                .whenPressed(() -> schedule(new InstantCommand(() -> touchpad.deleteLastInput())));
+                .whenPressed(() -> schedule(new InstantCommand(() -> {
+                    currentChoice = ChoiceState.RIGHT;
+                })));
+        liftPad.getGamepadButton(GamepadKeys.Button.LEFT_BUMPER)
+                .whenPressed(() -> schedule(new InstantCommand(() -> {
+                    currentChoice = ChoiceState.LEFT;
+                })));
 
         row = touchpad.getHistory().get(touchpad.getHistory().size() - 2);
         column = touchpad.getHistory().get(touchpad.getHistory().size() - 1);
+
+        liftPad.getGamepadButton(GamepadKeys.Button.Y)
+                .whenPressed(() -> new InstantCommand(() -> {
+
+                }));
 
         telemetry.addData("Status", "Ready!");
         telemetry.update();
