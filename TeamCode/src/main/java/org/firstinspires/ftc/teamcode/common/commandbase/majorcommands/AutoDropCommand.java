90a5d52b33fbb987ae4c79fef9d55ab690e9ff34
christopheraustin <arcticplayernoice@gmail.com>
2023-12-09 18:23:11 -0500

    autodrop update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/majorcommands/AutoDropCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/majorcommands/AutoDropCommand.java
@@ -4,6 +4,7 @@ import com.arcrobotics.ftclib.command.ParallelCommandGroup;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 
 import org.firstinspires.ftc.teamcode.common.commandbase.minorcommands.GrabberGripCommand;
+import org.firstinspires.ftc.teamcode.common.commandbase.minorcommands.LowerHorizontalMoveCommand;
 import org.firstinspires.ftc.teamcode.common.drive.MecanumDrive;
 import org.firstinspires.ftc.teamcode.common.subsystems.DepositSubsystem;
 import org.firstinspires.ftc.teamcode.common.subsystems.LiftSubsystem;
@@ -11,11 +12,12 @@ import org.firstinspires.ftc.teamcode.common.commandbase.majorcommands.SetReadyT
 import org.firstinspires.ftc.teamcode.common.vision.Camera;
 
     public class AutoDropCommand extends SequentialCommandGroup {
-    AutoDropCommand(DepositSubsystem depositSubsystem, LiftSubsystem liftSubsystem, LiftSubsystem.LiftHeight LiftHeight, Camera camera, MecanumDrive drive, DepositSubsystem.GrabberPos grabberPos){
+    AutoDropCommand(DepositSubsystem depositSubsystem, LiftSubsystem liftSubsystem, Camera camera, MecanumDrive drive, DepositSubsystem.GrabberPos grabberPos, DepositSubsystem.LowerHorizontalState lowerHorizontalState){
         addCommands(
                 new ParallelCommandGroup(
-                        new SetReadyToDeposit(depositSubsystem, liftSubsystem, LiftHeight),
-                        new ToTagCommand(camera, drive)
+                        new SetReadyToDeposit(depositSubsystem, liftSubsystem, LiftSubsystem.LiftHeight.HEIGHTONE),
+                        new ToTagCommand(camera, drive),
+                        new LowerHorizontalMoveCommand(depositSubsystem, lowerHorizontalState)
                 ),
                 new GrabberGripCommand(depositSubsystem, DepositSubsystem.GrabberState.OPEN, grabberPos)
         );
