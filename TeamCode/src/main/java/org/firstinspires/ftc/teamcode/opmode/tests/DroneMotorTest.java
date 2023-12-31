a47d9f20abce4282469ee4c7b503aa75d9310314
Matthew <matthewzhao105@gmail.com>
2023-12-30 20:58:33 -0500

    drone motor test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tests/DroneMotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tests/DroneMotorTest.java
@@ -1,11 +1,21 @@
 package org.firstinspires.ftc.teamcode.opmode.tests;
 
+import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.common.Robot;
+import org.firstinspires.ftc.teamcode.common.commandbase.minorcommands.DroneMotorTestCommand;
+import org.firstinspires.ftc.teamcode.common.subsystems.MiscSubsystem;
 
 public class DroneMotorTest extends LinearOpMode {
-
+    Robot robot;
+    MiscSubsystem miscSubsystem;
     @Override
     public void runOpMode() throws InterruptedException {
+        robot = new Robot(hardwareMap);
+        miscSubsystem = new MiscSubsystem(robot);
         waitForStart();
+        CommandScheduler.getInstance().schedule(
+                new DroneMotorTestCommand(miscSubsystem)
+        );
     }
 }
