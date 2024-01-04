c314b2ec03d36b0935b2bf40fd94ea8220a3dfec
Matthew <matthewzhao105@gmail.com>
2024-01-03 20:25:18 -0500

    Changes to the deposit subsystem, dc motor changed to servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/minorcommands/UpperHorizontalMoveCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/minorcommands/UpperHorizontalMoveCommand.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.common.commandbase.minorcommands;
 
 import com.arcrobotics.ftclib.command.CommandBase;
+import com.arcrobotics.ftclib.command.InstantCommand;
 
 import org.firstinspires.ftc.teamcode.common.subsystems.DepositSubsystem;
 
+/*
 public class UpperHorizontalMoveCommand extends CommandBase {
     DepositSubsystem.UpperHorizontalState desiredState;
     DepositSubsystem deposit;
@@ -21,5 +23,12 @@ public class UpperHorizontalMoveCommand extends CommandBase {
     @Override
     public boolean isFinished() {
         return !deposit.horizontalFinishedMoving();
+    }
+}
+*/
+
+public class UpperHorizontalMoveCommand extends InstantCommand {
+    public UpperHorizontalMoveCommand(DepositSubsystem deposit, DepositSubsystem.UpperHorizontalState horizontalState) {
+        deposit.setUpperHorizontalState(horizontalState);
     }
 }
