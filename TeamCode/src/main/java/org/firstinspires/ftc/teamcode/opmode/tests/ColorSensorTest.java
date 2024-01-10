65aa5b1f4cb5b1ee30b0a783b468ca9769d2a3b0
donaldzhuang <donaldzhuang08@gmail.com>
2024-01-09 20:56:59 -0500

    x axis test test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tests/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tests/ColorSensorTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.opmode.tests;
+/*package org.firstinspires.ftc.teamcode.opmode.tests;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -62,5 +62,33 @@ public class ColorSensorTest extends CommandOpMode {
         }
         telemetry.update();
         sleep(20);
+    }
+}*/
+package org.firstinspires.ftc.teamcode.opmode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class ColorSensorTest extends LinearOpMode {
+    // Define a variable for our color sensor
+    ColorSensor color;
+
+    @Override
+    public void runOpMode() {
+        // Get the color sensor from hardwareMap
+        color = hardwareMap.get(ColorSensor.class, "Color");
+
+        // Wait for the Play button to be pressed
+        waitForStart();
+
+        // While the Op Mode is running, update the telemetry values.
+        while (opModeIsActive()) {
+            telemetry.addData("Red", color.red());
+            telemetry.addData("Green", color.green());
+            telemetry.addData("Blue", color.blue());
+            telemetry.update();
+        }
     }
 }
