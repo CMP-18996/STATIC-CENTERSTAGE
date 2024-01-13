e7b39c650599c7dd44d91aa3733d58cd9a2bb481
CMP-18996 <calculusgeek11@gmail.com>
2024-01-13 09:15:56 -0500

    auto changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/BlueCloseAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/BlueCloseAuto.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode.opmode.autonomous;
 
-import static org.firstinspires.ftc.teamcode.common.subsystems.DepositSubsystem.UpperHorizontalState.C;
-
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.arcrobotics.ftclib.command.ConditionalCommand;
@@ -12,7 +10,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.common.GlobalVariables;
 import org.firstinspires.ftc.teamcode.common.Robot;
-import org.firstinspires.ftc.teamcode.common.commandbase.auto.ToTagCommand;
+import org.firstinspires.ftc.teamcode.common.commandbase.auto.ParkCommand;
 import org.firstinspires.ftc.teamcode.common.commandbase.auto.ToBoardCommand;
 import org.firstinspires.ftc.teamcode.common.commandbase.auto.ToSpikeMarkCommand;
 import org.firstinspires.ftc.teamcode.common.commandbase.majorcommands.AutoDropCommand;
@@ -24,7 +22,7 @@ import org.firstinspires.ftc.teamcode.common.subsystems.DepositSubsystem;
 import org.firstinspires.ftc.teamcode.common.subsystems.IntakeSubsystem;
 import org.firstinspires.ftc.teamcode.common.subsystems.LiftSubsystem;
 
-@Autonomous(name = "Blue Close")
+@Autonomous(name = "Autonomous")
 public class BlueCloseAuto extends CommandOpMode {
     public Robot robot;
     public MecanumDrive drive;
@@ -47,26 +45,52 @@ public class BlueCloseAuto extends CommandOpMode {
         depositSubsystem = new DepositSubsystem(robot);
         liftSubsystem = new LiftSubsystem(robot);
         super.register(robot.camera, intakeSubsystem, depositSubsystem, liftSubsystem);
-        //parallel command group op
         CommandScheduler.getInstance().schedule(
                 new SequentialCommandGroup(
                         new ToSpikeMarkCommand(drive),
                         new TakeFromIntakeCommand(liftSubsystem, depositSubsystem, intakeSubsystem),
                         new GroundDropCommand(depositSubsystem, liftSubsystem),
                         new FourBarCommand(depositSubsystem, DepositSubsystem.FourBarState.STASIS),
-                        new ToBoardCommand(drive),
-                        new WaitCommand(1000),
+                        new ToBoardCommand(drive),//switch with above if doing far side to go under chasis
+                        new WaitCommand(1000),//nessecary for camera to stabilize
                         new ConditionalCommand(new AutoDropCommand(depositSubsystem, liftSubsystem, robot.camera, drive, DepositSubsystem.LowerHorizontalState.A),
                                 new ConditionalCommand(new AutoDropCommand(depositSubsystem, liftSubsystem, robot.camera, drive, DepositSubsystem.LowerHorizontalState.E),
                                         new AutoDropCommand(depositSubsystem, liftSubsystem, robot.camera, drive, DepositSubsystem.LowerHorizontalState.C),
                                         () -> GlobalVariables.position == GlobalVariables.Position.RIGHT),
                                 () -> GlobalVariables.position == GlobalVariables.Position.LEFT),
+                        new FourBarCommand(depositSubsystem, DepositSubsystem.FourBarState.STASIS),
+                        new ParkCommand(drive),
                         new InstantCommand(() -> telemetry.addData("Status", "Complete!"))
                 )
         );
 
         robot.camera.startPropProcessing();
-        telemetry.addData("Status", "Initialized!");
+
+        telemetry.addData("Status", "Initialized! Please wait a bit before playing.");
+        switch (GlobalVariables.distance) {
+            case BLUECLOSE:
+                telemetry.addLine("You are registered for the blue close position.");
+                break;
+            case BLUEFAR:
+                telemetry.addLine("You are registered for the blue far position.");
+                break;
+            case REDCLOSE:
+                telemetry.addLine("You are registered for the red close position.");
+                break;
+            case REDFAR:
+                telemetry.addLine("You are registered for the red far position.");
+                break;
+        }
+        if ((GlobalVariables.distance == GlobalVariables.Distance.BLUECLOSE ||
+                GlobalVariables.distance == GlobalVariables.Distance.BLUEFAR) &&
+        GlobalVariables.color != GlobalVariables.Color.BLUE) {
+            telemetry.addLine("ERROR!!! It appears as if you are using the red setup in a blue position.");
+        }
+        if ((GlobalVariables.distance == GlobalVariables.Distance.REDCLOSE ||
+                GlobalVariables.distance == GlobalVariables.Distance.REDFAR) &&
+                GlobalVariables.color != GlobalVariables.Color.RED) {
+            telemetry.addLine("ERROR!!! It appears as if you are using the blue setup in a red position.");
+        }
         telemetry.update();
     }
     @Override
