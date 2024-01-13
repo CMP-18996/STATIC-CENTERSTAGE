e5aa01f953b7ab1a5bb130d360fdddfa9af2c707
Matthew <matthewzhao105@gmail.com>
2024-01-12 22:47:36 -0500

    hang stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/minorcommands/HangCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/minorcommands/HangCommand.java
@@ -5,9 +5,9 @@ import com.arcrobotics.ftclib.command.InstantCommand;
 import org.firstinspires.ftc.teamcode.common.subsystems.MiscSubsystem;
 
 import java.util.logging.Handler;
-/*
+
 public class HangCommand extends InstantCommand {
     public HangCommand(MiscSubsystem miscSubsystem) {
         miscSubsystem.hang();
     }
-} */
+}
