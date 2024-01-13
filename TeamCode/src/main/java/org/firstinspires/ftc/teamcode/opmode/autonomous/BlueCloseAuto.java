df7a798520d7c12f7b40ab9eed7f1508de2be46e
CMP-18996 <calculusgeek11@gmail.com>
2024-01-12 22:22:41 -0500

    auto changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/BlueCloseAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/BlueCloseAuto.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode.opmode.autonomous;
 
+import static org.firstinspires.ftc.teamcode.common.subsystems.DepositSubsystem.UpperHorizontalState.C;
+
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.command.ConditionalCommand;
 import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 import com.arcrobotics.ftclib.command.WaitCommand;
@@ -12,15 +15,21 @@ import org.firstinspires.ftc.teamcode.common.Robot;
 import org.firstinspires.ftc.teamcode.common.commandbase.auto.ToTagCommand;
 import org.firstinspires.ftc.teamcode.common.commandbase.auto.ToBoardCommand;
 import org.firstinspires.ftc.teamcode.common.commandbase.auto.ToSpikeMarkCommand;
+import org.firstinspires.ftc.teamcode.common.commandbase.majorcommands.AutoDropCommand;
+import org.firstinspires.ftc.teamcode.common.commandbase.majorcommands.GroundDropCommand;
+import org.firstinspires.ftc.teamcode.common.commandbase.majorcommands.TakeFromIntakeCommand;
 import org.firstinspires.ftc.teamcode.common.drive.MecanumDrive;
+import org.firstinspires.ftc.teamcode.common.subsystems.DepositSubsystem;
+import org.firstinspires.ftc.teamcode.common.subsystems.IntakeSubsystem;
+import org.firstinspires.ftc.teamcode.common.subsystems.LiftSubsystem;
 
 @Autonomous(name = "Blue Close")
 public class BlueCloseAuto extends CommandOpMode {
     public Robot robot;
     public MecanumDrive drive;
-    /*public IntakeSubsystem intakeSubsystem;
+    public IntakeSubsystem intakeSubsystem;
     public DepositSubsystem depositSubsystem;
-    public LiftSubsystem liftSubsystem;*/
+    public LiftSubsystem liftSubsystem;
     @Override
     public void initialize() {
         telemetry.addData("Status","Initalizing...");
@@ -33,19 +42,23 @@ public class BlueCloseAuto extends CommandOpMode {
         CommandScheduler.getInstance().reset();
         robot = new Robot(hardwareMap);
         drive = new MecanumDrive(hardwareMap, GlobalVariables.distance.getP());
-       /* intakeSubsystem = new IntakeSubsystem(robot);
+        intakeSubsystem = new IntakeSubsystem(robot);
         depositSubsystem = new DepositSubsystem(robot);
         liftSubsystem = new LiftSubsystem(robot);
-        super.register(robot.camera, intakeSubsystem, depositSubsystem, liftSubsystem);*/
+        super.register(robot.camera, intakeSubsystem, depositSubsystem, liftSubsystem);
         //parallel command group op
         CommandScheduler.getInstance().schedule(
                 new SequentialCommandGroup(
                         new ToSpikeMarkCommand(drive),
+                        new TakeFromIntakeCommand(liftSubsystem, depositSubsystem, intakeSubsystem),
                         new WaitCommand(1000),
-                        //new GroundDropCommand(),
+                        new GroundDropCommand(depositSubsystem, liftSubsystem),
                         new ToBoardCommand(drive),
-                        new ToTagCommand(robot.camera, drive),
-                        //new AutoDropCommand replaces tagcommand
+                        new ConditionalCommand(new AutoDropCommand(depositSubsystem, liftSubsystem, robot.camera, drive, DepositSubsystem.LowerHorizontalState.A),
+                                new ConditionalCommand(new AutoDropCommand(depositSubsystem, liftSubsystem, robot.camera, drive, DepositSubsystem.LowerHorizontalState.E),
+                                        new AutoDropCommand(depositSubsystem, liftSubsystem, robot.camera, drive, DepositSubsystem.LowerHorizontalState.C),
+                                        () -> GlobalVariables.position == GlobalVariables.Position.RIGHT),
+                                () -> GlobalVariables.position == GlobalVariables.Position.LEFT),
                         new InstantCommand(() -> telemetry.addData("Status", "Complete!"))
                 )
         );
