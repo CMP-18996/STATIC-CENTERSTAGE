2e0c3584c62f7118f7ba038e54a1538acdc15741
Matthew <matthewzhao105@gmail.com>
2024-02-03 16:42:22 -0500

    stasis stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/majorcommands/StasisCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/majorcommands/StasisCommand.java
@@ -24,7 +24,11 @@ public class StasisCommand extends ParallelCommandGroup {
         addCommands(
                 new IntakeCommand(intakeSubsystem, IntakeSubsystem.SweepingState.STOPPED),
                 new DepositRotatorCommand(depositSubsystem, DepositSubsystem.DepositRotationState.PICKING_UP),
-                new ZeroLiftCommand(liftSubsystem),
+                new LowerHorizontalMoveCommand(depositSubsystem, DepositSubsystem.LowerHorizontalState.C),
+                new ParallelDeadlineGroup(
+                        new WaitCommand(3000),
+                        new ZeroLiftCommand(liftSubsystem)
+                ),
                 new LowerHorizontalMoveCommand(depositSubsystem, DepositSubsystem.LowerHorizontalState.C),
                 new SequentialCommandGroup(
                         new FourBarCommand(depositSubsystem, DepositSubsystem.FourBarState.STASIS),
